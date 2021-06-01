d9aa7a304ee0b223a3cc989a8b4f7cc7fbd8fdf3
Akhil Murali Nair <github@akhilmurali.com>
2021-06-01 23:48:40 +0530

    allow cross origin access

--- dashboard/server.js
+++ dashboard/server.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', async (req, res) => {
     const client = new Client({
         user: 'vzmvmwyepoqpvl',
